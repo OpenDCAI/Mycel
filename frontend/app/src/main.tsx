1223afd915fd47862eab5fbe8765c396b51b6363
shuxueshuxue <lexicalmathical@gmail.com>
2026-04-07 19:50:54 +0800

    fix: stop duplicate resource fetches in app shell

--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
@@ -10,8 +9,8 @@ import './styles/effects.css'
 import { router } from './router.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <>
     <RouterProvider router={router} />
     <Toaster position="bottom-right" richColors />
-  </StrictMode>,
+  </>,
 )
